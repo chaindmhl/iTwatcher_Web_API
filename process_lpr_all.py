a0b1e73a461551b07c3d47dfbd791b17233f47cf
chaindmhl <117880660+chaindmhl@users.noreply.github.com>
2024-06-25 10:25:49 +0800

    Update process_lpr_all.py

--- process_lpr_all.py
+++ process_lpr_all.py
@@ -1,14 +1,15 @@
 from django.conf import settings
 from tracking.deepsort_tric.LPR_all import Plate_Recognition
-from tracking.models import LPRVideo
-import datetime, requests, tempfile, os, cv2, threading, queue
+import os, threading, queue
 
 
 REQUEST_URL = f"http://{settings.HOST}:8000/"
 
 
 def process_alllpr(video_path=None, livestream_url=None, is_live_stream=False, video_stream=None):
-    # Tricycle Detection and Tracking
+
+    processed_frames = []
+    
     if video_path:
         # Load the video file
         video_file = video_path
@@ -23,7 +24,7 @@ def process_alllpr(video_path=None, livestream_url=None, is_live_stream=False, v
         # Create an instance of the VehiclesCounting class
         pr = Plate_Recognition(file_counter_log_name='vehicle_count.log',
                               framework='tf',
-                              weights='/home/icebox/itwatcher_api/tracking/deepsort_tric/checkpoints/PlateDetection',
+                              weights='/home/icebox/itwatcher_api/tracking/deepsort_tric/checkpoints/LPD1',
                               size=416,
                               tiny=False,
                               model='yolov4',
@@ -41,80 +42,30 @@ def process_alllpr(video_path=None, livestream_url=None, is_live_stream=False, v
         # Start producer and consumer threads
         producer_thread = threading.Thread(target=pr.producer)
         consumer_thread = threading.Thread(target=pr.consumer)
-        showframe_thread = threading.Thread(target=pr.show_frames)
+
 
         producer_thread.start()
         consumer_thread.start()
-        showframe_thread.start()
 
-        # Wait for both threads to finish
-        producer_thread.join()
-        consumer_thread.join()
-        showframe_thread.join()
-        # Create an instance of ObjectTrack and save the video file to it
-        output_lpr = LPRVideo.objects.create(
-            video_block=output_video_path,
-        )
+        # Retrieve frames from the processed_queue in real-time
+        while producer_thread.is_alive() or consumer_thread.is_alive():
+            try:
+                processed_frame = pr._processedqueue.get(timeout=1)  # Wait for a frame for 1 second
+                yield processed_frame  # Yield the processed frame
+            except queue.Empty:
+                continue
 
-        # Create a temporary file for the video file
-        output_lpr_temp = tempfile.NamedTemporaryFile(delete=False, suffix='.mp4')
-        with open(output_video_path, 'rb') as f:
-            output_lpr_temp.write(f.read())
-        output_lpr_temp.close()
-
-        # Save the video file using FileField
-        output_lpr.video_lpr.save(os.path.basename(output_video_path), open(output_lpr_temp.name, 'rb'))
-
-        # Remove the temporary file
-        os.unlink(output_lpr_temp.name)
-
-    elif livestream_url:
-        # Check if the livestream_url is valid
-        response = requests.get(livestream_url, auth=('username', 'password'))
-        if response.status_code != 200:
-            # Handle invalid url    
-            return {'error': 'Invalid livestream url'}
-
-        # Create a VideoCapture object using the livestream url
-        video_file = cv2.VideoCapture(livestream_url)
-
-        # get the current timestamp
-        timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-
-        # create a unique output video name using the timestamp
-        output_video_path = os.path.join(output_folder_path, f'tracked_livestream_{timestamp}.avi')
-        os.makedirs(output_folder_path, exist_ok=True)
-
-        # Create an instance of the VehiclesCounting class
-        pr = Plate_Recognition(file_counter_log_name='vehicle_count.log',
-                              framework='tf',
-                              weights='/home/icebox/itwatcher_api/tracking/deepsort_tric/checkpoints/PlateDetection',
-                              size=416,
-                              tiny=False,
-                              model='yolov4',
-                              video=video_file,
-                              output=output_video_path,
-                              output_format='XVID',
-                              iou=0.45,
-                              score=0.5,
-                              dont_show=False,
-                              info=False,
-                              detection_line=(0.5, 0),
-                              frame_queue = queue.Queue(maxsize=1200),
-                              processed_queue=queue.Queue(maxsize=100))
+        # Ensure the threads are terminated
+        pr.stop()
         
-
-        # Start producer and consumer threads
-        producer_thread = threading.Thread(target=pr.producer)
-        consumer_thread = threading.Thread(target=pr.consumer)
-        showframe_thread = threading.Thread(target=pr.show_frames)
-
-        producer_thread.start()
-        consumer_thread.start()
-        showframe_thread.start()
-
         # Wait for both threads to finish
         producer_thread.join()
         consumer_thread.join()
-        showframe_thread.join()
+        print("stop threads")
 
+        # Retrieve any remaining frames in the queue
+        while not pr._processedqueue.empty():
+            processed_frame = pr._processedqueue.get()
+            yield processed_frame
+        
+    return processed_frames
